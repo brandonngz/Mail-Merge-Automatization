ec354db70d93370b2221f6296e94d727c7128a58
Brandon <brandonnz313@gmail.com>
2022-01-12 12:08:43 -0700

    first commit

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ with open("Input/Letters/starting_letter.txt") as letter_file:
         new_letter = letter_contents.replace("[name]", stripped_name)
         with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
+            
